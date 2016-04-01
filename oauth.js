899f06066b3d186eb2980587f0919d66c91c3ca9
thungasr <89589.thungathurti@students.itu.edu>
2016-04-01 10:50:42 -0700

    updated oAuth info

--- oauth.js
+++ oauth.js
@@ -5,8 +5,8 @@ var ids = {
     callbackURL: 'https://swe600-recording.herokuapp.com/auth/facebook/callback'
   },
   google: {
-    clientID: '989088271842-2u16eo2sboegvsec287jnhtmcsl1r7nn.apps.googleusercontent.com ',
-    clientSecret: 'KVkGD4EyN1fntLf8wVJvr3BN',
+    clientID: '1003631882203-h39iko7mudtca2h2mmqjpi1ekpac8q6s.apps.googleusercontent.com',
+    clientSecret: 'KWuXkAMVlf4aXuuXrYTK8cC0',
     returnURL: 'https://swe600-recording.herokuapp.com/auth/google/callback'
   },
    linkedin: {
